6eca589e1941b51afca0ff8e0cbe44efb989778c
Danielle Smith <danini@hey.com>
2022-07-01 13:02:39 +0200

    first commit

---
+++ test/test_xml_in_kdl.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestXmlInKdl < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::XmlInKdl::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
